964c5902243446124417a3a9e9454f5ef032cc36
rockerBOO <rockerboo@gmail.com>
2024-05-17 14:04:15 -0400

    Fix deprecation of buf_get_clients to get_clients

--- lua/symbols-outline/utils/lsp_utils.lua
+++ lua/symbols-outline/utils/lsp_utils.lua
@@ -1,15 +1,14 @@
 local config = require 'symbols-outline.config'
-local tbl_utils = require 'symbols-outline.utils.table'
 
 local M = {}
 
 function M.is_buf_attached_to_lsp(bufnr)
-  local clients = vim.lsp.buf_get_clients(bufnr or 0)
+  local clients = vim.lsp.get_clients({ bufnr = bufnr or 0 })
   return clients ~= nil and #clients > 0
 end
 
 function M.is_buf_markdown(bufnr)
-  return vim.api.nvim_buf_get_option(bufnr, 'ft') == 'markdown'
+  return vim.api.nvim_get_option_value('ft', { buf = bufnr}) == 'markdown'
 end
 
 --- Merge all client token lists in an LSP response
